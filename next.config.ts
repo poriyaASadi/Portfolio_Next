d95903581c545fdf289eaf8ee179cbdf8a9cb67c
poriyaASadi <133978734+poriyaASadi@users.noreply.github.com>
2025-12-26 01:23:32 +0330

    add code

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  typescript: {
+    // غیرفعال کردن خطاهای تایپ‌اسکریپت در زمان build
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // اختیاری: اگر خطای ESLint هم می‌خوای نادیده بگیری
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
